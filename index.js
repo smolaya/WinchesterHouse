b8c9897cf7d0d63af0f1d3083c91bb07d74e0a2d
Stephanie Olaya <stepholaya@gmail.com>
2018-11-04 21:07:30 -0500

    saved changes

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 let  readlineSync  =  require('readline-sync');;
-let randomNumber = Math.round((Math.random() * 10));
 
-let guess = readlineSync.question('Guess a number between 1 through 10');
+let name = readlineSync.question ("What is your name?");
 
-if (guess == randomNumber){
+let start = readlineSync.question ( "Welcome to Winchester House " + name + ". Do you have what it takes to get out?" );
 
- console.log('you guessed right!');
+if (start === "yes") {
+console.log( "Choose a room " + name );
+} else {
+  console.log ("Come back when you're ready for a challange...")
+}
 
-}  else {
 
-  console.log ('Sorry. The number was ' + randomNumber + ' ');
+
+let option = readlineSync. question ( "Option:  room 1, room 2, room 3" );
+
+switch(option) {
+  case "room 1":
+    console.log( "You got lucky! This is the room that holds the exit door! You've escaped Winchester House" );
+    break;
+  case "room 2":
+    console.log( "Oh no! You've gotten locked in the room with no return!" );
+    break;
+  case "room 3":
+    console.log( "The room has a window that leads to the garden, break it and see what happens!" );
+    break;
+  default:
+    console.log( "Oh no, you got lost in Winchester House, please try again!" );
 
 }
